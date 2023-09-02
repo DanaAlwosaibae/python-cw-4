f2274b859b1fe66360f83d889b5dbcfac337011d
DanaAlwosaibae <142634164+DanaAlwosaibae@users.noreply.github.com>
2023-09-02 20:08:17 +0300

    done

---
+++ cw4.py
@@ -0,0 +1,23 @@
+def my_name():
+    print('my name is dana')
+
+my_name()  
+
+def my_meal(food,drink):
+    
+
+    print(f'i like eat {food} and whail drinking {drink}')
+
+my_meal('pezza','water')
+
+def cupe(number):
+        return number**3
+
+def by_three(number):
+     if number%3==0:
+          return cupe(number)
+     else:
+          return False
+print(cupe(3))
+print(by_three(5))
+
